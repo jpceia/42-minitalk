d120641b63a0254afcbb7adaded25b54bc036cc0
jpceia <joao.p.ceia@gmail.com>
2021-08-30 21:50:28 +0100

    updated server.c

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 01:13:34 by jpceia            #+#    #+#             */
-/*   Updated: 2021/08/30 20:25:51 by jceia            ###   ########.fr       */
+/*   Updated: 2021/08/30 21:48:47 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,32 @@
 #include <signal.h>
 #include <stdio.h>
 
-void	handle_sigusr(int sig, siginfo_t *info, void *ucontext)
+static void	handle_srv_sigusr(int sig, siginfo_t *info, void *ucontext)
 {
-	static int	nbits = 0;
-	static char	c = 0;
-	static char	*msg = NULL;
+	static pid_t	pid;
+	static int		nbits = 0;
+	static char		c = 0;
+	static char		*msg = NULL;
 
 	(void)ucontext;
+
+	if (info->si_pid)
+	{
+		ft_putstr("Updated pid: ");
+		ft_putnbr(info->si_pid);
+		ft_putchar('\n');
+		pid = info->si_pid;
+	}
 	if (sig != SIGUSR1 && sig != SIGUSR2)
 		return ;
 	c <<= 1;
 	if (sig == SIGUSR2)
 		c++;
+	ft_putstr("Received signal: ");
+	ft_putnbr(sig);
+	ft_putchar(' ');
+	ft_putchar(c);
+	ft_putchar('\n');
 	if (++nbits % 8 == 0)
 	{
 		if (c)
@@ -35,21 +49,18 @@ void	handle_sigusr(int sig, siginfo_t *info, void *ucontext)
 		{
 			ft_putstr(msg);
 			free(msg);
+			if (kill(pid, SIGUSR2) < 0)
+			{
+				ft_putendl_fd("Error sending SIGUSR2 signal to client", STDERR_FILENO);
+				exit(EXIT_FAILURE);
+			}
+			return ;
 		}
 		c = 0;
 	}
-}
-
-static void	set_sigusr(struct sigaction *sa)
-{
-	if (sigaction(SIGUSR1, sa, NULL) < 0)
+	if (kill(pid, SIGUSR1) < 0)
 	{
-		ft_putendl("Failed to setup SIGUSR1 handler");
-		exit(EXIT_FAILURE);
-	}
-	if (sigaction(SIGUSR2, sa, NULL) < 0)
-	{
-		ft_putendl("Failed to setup SIGUSR2 handler");
+		ft_putendl_fd("Error sending SIGUSR1 signal to client", STDERR_FILENO);
 		exit(EXIT_FAILURE);
 	}
 }
@@ -63,7 +74,7 @@ int	main(void)
 	sigaddset(&sa.sa_mask, SIGQUIT);
 	sa.sa_handler = 0;
 	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = handle_sigusr;
+	sa.sa_sigaction = handle_srv_sigusr;
 	set_sigusr(&sa);
 	ft_putstr("Server PID: ");
 	ft_putnbr(getpid());
