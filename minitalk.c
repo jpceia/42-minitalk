8aca1fc3ccf26f7c7c0d7aa243de7e137d1ed104
jpceia <joao.p.ceia@gmail.com>
2021-08-31 00:30:06 +0100

    added "send bit" function

--- minitalk.c
+++ minitalk.c
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:16:08 by jceia             #+#    #+#             */
-/*   Updated: 2021/08/30 21:21:09 by jceia            ###   ########.fr       */
+/*   Updated: 2021/08/31 00:25:53 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,25 @@ void	set_sigusr(struct sigaction *sa)
 	{
 		ft_putendl("Failed to setup SIGUSR2 handler");
 		exit(EXIT_FAILURE);
+	}
+}
+
+void	send_bit(pid_t pid, bool b)
+{
+	if (b)
+	{
+		if (kill(pid, SIGUSR2) < 0)
+		{
+			ft_putendl_fd("Error sending signal SIGUSR2", STDERR_FILENO);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (kill(pid, SIGUSR1) < 0)
+		{
+			ft_putendl_fd("Error sending signal SIGUSR1", STDERR_FILENO);
+			exit(EXIT_FAILURE);
+		}
 	}
 }
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:16:11 by jceia             #+#    #+#             */
-/*   Updated: 2021/08/30 21:21:05 by jceia            ###   ########.fr       */
+/*   Updated: 2021/08/31 00:26:10 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define MINITALK_H
 
 # include <signal.h>
+# include <stdbool.h>
 
 void	set_sigusr(struct sigaction *sa);
+void	send_bit(pid_t pid, bool b);
 
 #endif
